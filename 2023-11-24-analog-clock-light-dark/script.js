eb7c86627ded26a40c2384a62108ea4da4f692b6
youssefhafezzzzz <youssefhafez57@gmail.com>
2023-11-24 16:39:27 +0200

    update directories

---
+++ 2023-11-24-analog-clock-light-dark/script.js
@@ -0,0 +1,46 @@
+const deg = 6;
+const hour = document.querySelector(".hour");
+const min = document.querySelector(".min");
+const sec = document.querySelector(".sec");
+
+const setClock = () => {
+  let day = new Date();
+  let hh = day.getHours() * 30;
+  let mm = day.getMinutes() * deg;
+  let ss = day.getSeconds() * deg;
+
+  hour.style.transform = `rotateZ(${hh + mm / 12}deg)`;
+  min.style.transform = `rotateZ(${mm}deg)`;
+  sec.style.transform = `rotateZ(${ss}deg)`;
+};
+
+// first time
+setClock();
+// Update every 1000 ms
+setInterval(setClock, 1000);
+
+const switchTheme = (evt) => {
+  const switchBtn = evt.target;
+  if (switchBtn.textContent.toLowerCase() === "light") {
+    switchBtn.textContent = "dark";
+    // localStorage.setItem("theme", "dark");
+    document.documentElement.setAttribute("data-theme", "dark");
+  } else {
+    switchBtn.textContent = "light";
+    // localStorage.setItem("theme", "light"); //add this
+    document.documentElement.setAttribute("data-theme", "light");
+  }
+};
+
+const switchModeBtn = document.querySelector(".switch-btn");
+switchModeBtn.addEventListener("click", switchTheme, false);
+
+let currentTheme = "dark";
+// currentTheme = localStorage.getItem("theme")
+// 	? localStorage.getItem("theme")
+// 	: null;
+
+if (currentTheme) {
+  document.documentElement.setAttribute("data-theme", currentTheme);
+  switchModeBtn.textContent = currentTheme;
+}
